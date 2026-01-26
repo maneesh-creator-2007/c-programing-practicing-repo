7deb8ba9350450803e895f177d84049ce098aec1
Guru Maneesh Kumar <gurumaneeshkumardora@gmail.com>
2026-01-26 23:14:09 +0530

    added sum of the two number code

---
+++ sum-of-two-number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a,b,sum;
+    printf("enter two integer values:");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("the sum of the two integers is :%d\n",sum); 
+    return 0;
+}
