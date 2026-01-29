e2fc2b71c923710f184f242e03159a855bddcc72
Guru Maneesh Kumar <gurumaneeshkumardora@gmail.com>
2026-01-29 22:02:22 +0530

    added the long code

---
+++ basics-of-c.c/long.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+    int a;
+    long int b;
+    printf("enter the values of a and b:");
+    scanf("%d %ld",&a,&b);
+    printf("the value of a is %d and the value of b is %d",a,b);
+    return 0;
+    }
