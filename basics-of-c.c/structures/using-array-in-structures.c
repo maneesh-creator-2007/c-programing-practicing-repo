d1d4aaa700777e34231bb85d3f81292ecdb8a828
Guru Maneesh Kumar <gurumaneeshkumardora@gmail.com>
2026-03-04 20:56:17 +0530

    added code

---
+++ basics-of-c.c/structures/using-array-in-structures.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+struct one{
+    int a[2][2];
+}arr;
+int main(){
+printf("enter a number to store at a[1][1]:");
+scanf("%d",&arr.a[1][1]);
+printf("the entered number is %d\n",arr.a[1][1]);
+return 0;
+}
